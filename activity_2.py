db362682b1f12ba82c843e19cb66d77c660dffda
thelasttenno <66742986+thelasttenno@users.noreply.github.com>
2020-06-29 14:19:26 -0700

    stupid python

---
+++ activity_2.py
@@ -0,0 +1,27 @@
+nam = input('Enter Your Name ')
+print('Hello', nam)
+
+hours = input('Input Hours Worked:')
+rate = input('Input Rate Per Hour:')
+pay = float(hour) * flaot(rate)
+print("Total Pay: " + str(pay))
+
+
+# width = int(17)
+# height = float(12.0)
+# one = width//2
+# two = width/2.0
+# three = height/3
+# four = 1 + 2 * 5
+# str(five) = “cat” + “dog”
+# print('1 = ' + str(one))
+# type(one)
+# print('2 = ' + str(two))
+# type(two)
+# print('3 = ' + str(three))
+# type(three)
+# print('4 = ' + str(four))
+# type(four)
+# print('5 = ' + str(five))
+# type(five)
+# wait = input("PRESS ENTER TO CONTINUE.")
