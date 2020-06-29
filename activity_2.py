cbec5fb4a0ed6dbbfb8d53bed262188ccbcd48bc
thelasttenno <66742986+thelasttenno@users.noreply.github.com>
2020-06-29 14:50:36 -0700

    activity 2 finished

--- activity_2.py
+++ activity_2.py
@@ -1,27 +1,37 @@
 nam = input('Enter Your Name ')
 print('Hello', nam)
 
+wait = input("PRESS ENTER TO CONTINUE.")
+
 hours = input('Input Hours Worked:')
 rate = input('Input Rate Per Hour:')
-pay = float(hour) * flaot(rate)
+pay = float(hours) * float(rate)
 print("Total Pay: " + str(pay))
 
+wait = input("PRESS ENTER TO CONTINUE.")
 
-# width = int(17)
-# height = float(12.0)
-# one = width//2
-# two = width/2.0
-# three = height/3
-# four = 1 + 2 * 5
-# str(five) = “cat” + “dog”
-# print('1 = ' + str(one))
-# type(one)
-# print('2 = ' + str(two))
-# type(two)
-# print('3 = ' + str(three))
-# type(three)
-# print('4 = ' + str(four))
-# type(four)
-# print('5 = ' + str(five))
-# type(five)
-# wait = input("PRESS ENTER TO CONTINUE.")
+width = int(17)
+height = float(12.0)
+one = width//2
+two = width/2.0
+three = height/3
+four = 1 + 2 * 5
+five = str('cat' + 'dog')
+print('1 = ' + str(one))
+print(type(one))
+print('2 = ' + str(two))
+print(type(two))
+print('3 = ' + str(three))
+print(type(three))
+print('4 = ' + str(four))
+print(type(four))
+print('5 = ' + str(five))
+print(type(five))
+
+wait = input("PRESS ENTER TO CONTINUE.")
+
+tempC = input('Enter a Temperature in Celsius: ')
+tempF = ((float(tempC) * 9/5) + 32)
+print('Temperature in Fahrenheit: ' + str(tempF))
+
+wait = input("PRESS ENTER TO CONTINUE.")
